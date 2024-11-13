81486b39270f6faf63753fd0fcb6637afd329384
IlyaSchelev <160210145+IlyaSchelev@users.noreply.github.com>
2024-11-13 14:26:24 +0300

    Update habits-board-presenter.js

--- src/presenter/habits-board-presenter.js
+++ src/presenter/habits-board-presenter.js
@@ -1,21 +1,34 @@
 // habits-board-presenter.js
 import HabitsModel from '../model/habits-model.js';
 import HabitsListComponent from '../view/habits-list-component.js';
+import FilterHabitComponent from '../view/filter-habits-component.js';  // Компонент фильтрации
 
 export default class HabitsBoardPresenter {
-  constructor({ boardContainer }) {
+  constructor({ boardContainer, formContainer }) {
     this.boardContainer = boardContainer;
+    this.formContainer = formContainer;
     this.habitsModel = new HabitsModel();
     this.habitsListComponent = new HabitsListComponent();
+    this.filterStatus = 'all'; // Статус фильтра по умолчанию
   }
 
   init() {
+    this.renderFilterComponent();
     this.renderHabitsList();
     this.setAddHabitHandler();
   }
 
+  renderFilterComponent() {
+    const filterComponent = new FilterHabitComponent();
+    filterComponent.getElement().querySelector('#status-filter').addEventListener('change', (event) => {
+      this.filterStatus = event.target.value;
+      this.renderHabitsList(); // Обновить список привычек при изменении фильтра
+    });
+    this.boardContainer.appendChild(filterComponent.getElement());
+  }
+
   renderHabitsList() {
-    const habits = this.habitsModel.getHabits();
+    const habits = this.habitsModel.getHabits(this.filterStatus);
     this.habitsListComponent.getElement().querySelector('#habit-list').innerHTML = ''; // Очистить текущий список
     habits.forEach(habit => this.habitsListComponent.renderHabit(habit, this.handleDeleteHabit.bind(this), this.handleEditHabit.bind(this), this.handleStatusChange.bind(this)));
     this.boardContainer.appendChild(this.habitsListComponent.getElement());
