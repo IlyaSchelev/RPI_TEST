590e3bc297113cc0dea2f707caa2f51ec0c77b07
IlyaSchelev <160210145+IlyaSchelev@users.noreply.github.com>
2024-11-13 14:26:49 +0300

    Update filter-habits-component.js

--- src/view/filter-habits-component.js
+++ src/view/filter-habits-component.js
@@ -1,36 +1,31 @@
+// filter-habits-component.js
 import { createElement } from '../framework/render.js';
 
-
 function createFilterBookComponentTemplate() {
-    return (
-        `<div class="habit-filter">
-            <h2>Фильтры</h2>
-            <label for="status-filter">Фильтр по статусу:</label>
-            <select id="status-filter">
-                <option value="all">Все</option>
-                <option value="active">Активные</option>
-                <option value="completed">Завершенные</option>
-            </select>
-        </div>`
-      );
+  return (
+    `<div class="habit-filter">
+        <h2>Фильтры</h2>
+        <label for="status-filter">Фильтр по статусу:</label>
+        <select id="status-filter">
+            <option value="all">Все</option>
+            <option value="nodone">Активные</option>
+            <option value="done">Завершенные</option>
+        </select>
+    </div>`
+  );
 }
 
-
-export default class FilterBookComponent {
+export default class FilterHabitComponent {
   getTemplate() {
     return createFilterBookComponentTemplate();
   }
-
 
   getElement() {
     if (!this.element) {
       this.element = createElement(this.getTemplate());
     }
-
-
     return this.element;
   }
-
 
   removeElement() {
     this.element = null;
